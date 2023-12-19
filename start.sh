b845a723a050d7b9fc3fb19452b2aaa54108d2ad
nemo <git@ningu.net>
2023-12-19 17:46:16 +0100

    Make device configurable

--- start.sh
+++ start.sh
@@ -2,7 +2,13 @@
 
 source openvino/l_openvino_toolkit_debian9_2023.2.0.13089.cfd42bd2cb0_arm64/setupvars.sh
 
+dev=0
+
+if [ "$#" -gt 1 ]; then
+    dev="$1"
+fi
+
 poetry run python vmse2kv3/detector.py \
-        --device 0 \
+        --device "$dev" \
         --model pywhispercpp/extern/whispercpp/models/ggml-base.bin \
         --audio-ctx 512
